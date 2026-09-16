22e73731d86074685600291508327db1f1835f90
chapman39 <chapman39@llnl.gov>
2026-09-16 11:39:28 -0700

    store flag in variable to reduce duplication

--- cmake/thirdparty/BLTSetupMPI.cmake
+++ cmake/thirdparty/BLTSetupMPI.cmake
@@ -30,6 +30,11 @@ set(_mpi_compile_flags )
 set(_mpi_includes )
 set(_mpi_libraries )
 set(_mpi_link_flags )
+set(_cuda_host_flag_prefix "")
+
+if(BLT_ENABLE_CUDA AND NOT BLT_ENABLE_CLANG_CUDA)
+    set(_cuda_host_flag_prefix "-Xcompiler=")
+endif()
 
 
 if(BLT_ENABLE_FIND_MPI)
@@ -51,15 +56,9 @@ if (BLT_ENABLE_FIND_MPI)
     #-------------------
     set(_c_flag ${MPI_C_${_mpi_compile_flags_suffix}})
     if (_c_flag AND BLT_ENABLE_CUDA)
-        if(BLT_ENABLE_CLANG_CUDA)
-            list(APPEND _mpi_compile_flags
-                        $<$<NOT:$<COMPILE_LANGUAGE:CUDA>>:${_c_flag}>
-                        $<$<COMPILE_LANGUAGE:CUDA>:${_c_flag}>)
-        else()
-            list(APPEND _mpi_compile_flags
-                        $<$<NOT:$<COMPILE_LANGUAGE:CUDA>>:${_c_flag}>
-                        $<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=${_c_flag}>)
-        endif()
+        list(APPEND _mpi_compile_flags
+                    $<$<NOT:$<COMPILE_LANGUAGE:CUDA>>:${_c_flag}>
+                    $<$<COMPILE_LANGUAGE:CUDA>:${_cuda_host_flag_prefix}${_c_flag}>)
     else()
         list(APPEND _mpi_compile_flags ${_c_flag})
     endif()
@@ -67,15 +66,9 @@ if (BLT_ENABLE_FIND_MPI)
     set(_cxx_flag ${MPI_CXX_${_mpi_compile_flags_suffix}})
     if (_cxx_flag AND NOT "${_c_flag}" STREQUAL "${_cxx_flag}")
         if (BLT_ENABLE_CUDA)
-            if(BLT_ENABLE_CLANG_CUDA)
-                list(APPEND _mpi_compile_flags
-                $<$<NOT:$<COMPILE_LANGUAGE:CUDA>>:${_cxx_flag}>
-                $<$<COMPILE_LANGUAGE:CUDA>:${_cxx_flag}>)
-            else()
-                list(APPEND _mpi_compile_flags
-                $<$<NOT:$<COMPILE_LANGUAGE:CUDA>>:${_cxx_flag}>
-                $<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=${_cxx_flag}>)
-            endif()
+            list(APPEND _mpi_compile_flags
+            $<$<NOT:$<COMPILE_LANGUAGE:CUDA>>:${_cxx_flag}>
+            $<$<COMPILE_LANGUAGE:CUDA>:${_cuda_host_flag_prefix}${_cxx_flag}>)
         else()
             list(APPEND _mpi_compile_flags ${_cxx_flag})
         endif()
