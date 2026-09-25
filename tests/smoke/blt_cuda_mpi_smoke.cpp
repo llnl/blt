79f0ef0498668cae7c97a05afc71ad906c9b1689
Chris White <white238@llnl.gov>
2026-09-25 15:26:26 -0700

    remove complicated cuda+mpi test that didnt add value over the cuda+mpi smoke test, also simplify that

--- tests/smoke/blt_cuda_mpi_smoke.cpp
+++ tests/smoke/blt_cuda_mpi_smoke.cpp
@@ -3,66 +3,38 @@
 //
 // SPDX-License-Identifier: (BSD-3-Clause)
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-// Note: Parts of this are a CUDA Hello world example from NVIDIA:
-// Obtained from here: https://developer.nvidia.com/cuda-education
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-
 //-----------------------------------------------------------------------------
 //
 // file: blt_cuda_mpi_smoke.cpp
 //
 //-----------------------------------------------------------------------------
 
-#include <mpi.h>
+#include <cuda_runtime.h>
 #include <iostream>
+#include <mpi.h>
 #include <stdio.h>
 
-__device__ const char *STR = "HELLO WORLD!";
-const char STR_LENGTH = 12;
+__global__ void hello(int rank) { printf("Hello from MPI rank %d\n", rank); }
 
-__global__ void hello()
-{
-  printf("%c\n", STR[threadIdx.x % STR_LENGTH]);
-}
-
-int main(int argc, char** argv)
-{
-  // CUDA smoke test
-  int num_threads = STR_LENGTH;
-  int num_blocks = 1;
-  hello<<<num_blocks,num_threads>>>();
-  cudaDeviceSynchronize();
-
-  // MPI smoke test
-  // Initialize MPI and get rank and comm size
+int main(int argc, char **argv) {
   MPI_Init(&argc, &argv);
 
-  int commRank = -1;
-  MPI_Comm_rank(MPI_COMM_WORLD, &commRank);
-  int commSize = -1;
-  MPI_Comm_size(MPI_COMM_WORLD, &commSize);
+  int rank = -1;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  // Do a basic mpi reduce to determine this actually works
-  int globalValue = 0;
-  int valueToSend = 1;
-  MPI_Reduce(&valueToSend, &globalValue, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  hello<<<1, 1>>>(rank);
+  cudaError_t status = cudaDeviceSynchronize();
 
-  // Finalize MPI
-  MPI_Finalize();
+  int localSuccess = status == cudaSuccess;
+  int globalSuccess = 0;
+  MPI_Allreduce(&localSuccess, &globalSuccess, 1, MPI_INT, MPI_MIN,
+                MPI_COMM_WORLD);
 
-  if (commRank == 0)
-  {
-    std::cout << "Count should be equal to rank size" << std::endl;
-    std::cout << "Count = " << globalValue << ", Size = " << commSize << std::endl;
-
-    if (globalValue != commSize)
-    {
-      return 1;
-    }
+  if (!localSuccess) {
+    std::cerr << "MPI rank " << rank << ": " << cudaGetErrorString(status)
+              << std::endl;
   }
 
-  return 0;
+  MPI_Finalize();
+  return globalSuccess ? 0 : 1;
 }
-
-
